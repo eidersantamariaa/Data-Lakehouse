0802c02a443249c16ec4078dfe6eeaa79c2682e0
eidersantamariaa <eidersantamaria17@gmail.com>
2026-03-24 11:36:45 +0100

    .

--- ingesta.py
+++ ingesta.py
@@ -8,8 +8,8 @@ def get_spark():
     conf = (
         pyspark.SparkConf()
         .setAppName('ingesta')
-        .set('spark.jars.packages', 'org.apache.iceberg:iceberg-spark-runtime-4.0_2.13:1.8.1,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.367')
-        .set('spark.sql.extensions', 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions')
+        .set('spark.jars.packages', 'org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.8.1,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.367')
+        #.set('spark.sql.extensions', 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions')
         .set("spark.sql.catalog.players", "org.apache.iceberg.spark.SparkCatalog")
         .set("spark.sql.catalog.players.type", "rest")
         .set("spark.sql.catalog.players.uri", "http://172.16.58.11:32688")
