e61648dc0b8809b0a15adc205208e206cd49ad68
eidersantamariaa <eidersantamaria17@gmail.com>
2026-03-24 12:20:41 +0100

    .

--- ingesta.py
+++ ingesta.py
@@ -5,36 +5,48 @@ import pyspark
 from audit_log import write_audit_log
 
 def get_spark():
-    conf = (
-        pyspark.SparkConf()
-        .setAppName('ingesta')
-        .set('spark.jars.packages', 'org.apache.iceberg:iceberg-spark-runtime-4.0_2.13:1.8.1,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.367')
-        #.set('spark.sql.extensions', 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions')
-        .set("spark.sql.catalog.players", "org.apache.iceberg.spark.SparkCatalog")
-        .set("spark.sql.catalog.players.type", "rest")
-        .set("spark.sql.catalog.players.uri", "http://172.16.58.11:32688")
-        .set("spark.sql.catalog.players.warehouse", "s3://warehouse/")
-        .set("spark.sql.catalog.players.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
-        .set("spark.sql.catalog.players.s3.endpoint", "http://172.16.58.11:31224")
-        .set("spark.sql.catalog.players.s3.region", "us-east-1")
-        .set("spark.sql.catalog.players.s3.path-style-access", "true")
-        .set("spark.sql.catalog.players.client.region", "us-east-1")
-        .set("spark.hadoop.fs.s3a.endpoint", "http://172.16.58.11:31224")
-        .set("spark.hadoop.fs.s3a.endpoint.region", "us-east-1")
-        .set("spark.hadoop.fs.s3a.access.key", "GK5f421d5f440758f74b0e0312")
-        .set("spark.hadoop.fs.s3a.secret.key", "409baa63477885db12cd1db0a518748c5e83e971b5e8cf2129fe6c7498de125d")
-        .set("spark.hadoop.fs.s3a.path.style.access", "true")
-        .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-        .set("spark.sql.catalog.players.s3.checksum-algorithm", "NONE")
-        .set("spark.sql.catalog.players.s3.streaming-upload-enabled", "false")
-        .set("spark.sql.catalog.players.s3.chunked-encoding-enabled", "false")
-        .set("spark.sql.catalog.players.s3.payload-signing-enabled", "true")
-        .set("spark.sql.catalog.players.s3.http-client-type", "urlconnection")
-        .set("spark.executor.extraJavaOptions", "-Daws.requestChecksumCalculation=when_required")
-        .set("spark.driver.extraJavaOptions", "-Daws.requestChecksumCalculation=when_required")
-        
+    builder = SparkSession.builder.appName("ingesta")
+
+    builder = builder.config(
+        "spark.jars.packages",
+        "org.apache.iceberg:iceberg-spark-runtime-4.0_2.13:1.8.1,"
+        "org.apache.hadoop:hadoop-aws:3.3.4,"
+        "com.amazonaws:aws-java-sdk-bundle:1.12.367"
     )
-    return SparkSession.builder.config(conf=conf).getOrCreate()
+
+    # Iceberg catalog
+    builder = builder.config("spark.sql.catalog.players", "org.apache.iceberg.spark.SparkCatalog")
+    builder = builder.config("spark.sql.catalog.players.type", "rest")
+    builder = builder.config("spark.sql.catalog.players.uri", "http://172.16.58.11:32688")
+    builder = builder.config("spark.sql.catalog.players.warehouse", "s3://warehouse/")
+    builder = builder.config("spark.sql.catalog.players.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+
+    # S3 / Iceberg S3 config
+    builder = builder.config("spark.sql.catalog.players.s3.endpoint", "http://172.16.58.11:31224")
+    builder = builder.config("spark.sql.catalog.players.s3.region", "us-east-1")
+    builder = builder.config("spark.sql.catalog.players.s3.path-style-access", "true")
+    builder = builder.config("spark.sql.catalog.players.client.region", "us-east-1")
+
+    # Hadoop S3A config
+    builder = builder.config("spark.hadoop.fs.s3a.endpoint", "http://172.16.58.11:31224")
+    builder = builder.config("spark.hadoop.fs.s3a.endpoint.region", "us-east-1")
+    builder = builder.config("spark.hadoop.fs.s3a.access.key", "GK5f421d5f440758f74b0e0312")
+    builder = builder.config("spark.hadoop.fs.s3a.secret.key", "409baa63477885db12cd1db0a518748c5e83e971b5e8cf2129fe6c7498de125d")
+    builder = builder.config("spark.hadoop.fs.s3a.path.style.access", "true")
+    builder = builder.config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+    # Iceberg S3 tuning
+    builder = builder.config("spark.sql.catalog.players.s3.checksum-algorithm", "NONE")
+    builder = builder.config("spark.sql.catalog.players.s3.streaming-upload-enabled", "false")
+    builder = builder.config("spark.sql.catalog.players.s3.chunked-encoding-enabled", "false")
+    builder = builder.config("spark.sql.catalog.players.s3.payload-signing-enabled", "true")
+    builder = builder.config("spark.sql.catalog.players.s3.http-client-type", "urlconnection")
+
+    # Java options
+    builder = builder.config("spark.executor.extraJavaOptions", "-Daws.requestChecksumCalculation=when_required")
+    builder = builder.config("spark.driver.extraJavaOptions", "-Daws.requestChecksumCalculation=when_required")
+
+    return builder.getOrCreate()
 
 
 def run_ingesta(config):
