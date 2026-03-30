d71665870952b55f4257d3de3bda0e3f11821a13
eidersantamariaa <eidersantamaria17@gmail.com>
2026-03-30 09:39:33 +0200

    .

--- transfermarkt_bronce.py
+++ transfermarkt_bronce.py
@@ -9,7 +9,7 @@ from pyspark.sql import SparkSession
 import pyspark
 import time
 
-base_url = "http://172.25.240.1:8000"
+base_url = "http://172.25.240.1:8080"
 session = requests.Session()
 
 NAMESPACE = "transfermarkt"
