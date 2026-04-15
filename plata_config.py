922f5e734243e4c84469b426b8800a32f7933381
eidersantamariaa <eidersantamaria17@gmail.com>
2026-04-15 14:41:23 +0200

    Refactor: Update player transformation logic to align with new data schema and enhance normalization functions

--- plata_config.py
+++ plata_config.py
@@ -5,34 +5,36 @@ NAMESPACE = "mapping"
 
 SILVER_TRANSFORMS = {
     "players": lambda df: clean_basic(df).select(
-        col("id"),
-        normalize_text_udf(col("name")).alias("name"),
-        normalize_text_udf(col("fullName")).alias("fullName"),
-        normalize_date_udf(col("dateOfBirth")).alias("dateOfBirth"),
-        normalize_text_udf(col("nameInHomeCountry")).alias("nameInHomeCountry"),
-        normalize_height_udf(col("height")).alias("height"),
-        normalize_text_udf(col("foot")).alias("foot"),
-        col("shirtNumber"),
-        normalize_text_udf(normalize_currency_udf(col("marketValue"))).alias("marketValue (€)"),
-        col("isRetired").cast("boolean"),
-        normalize_text_udf(array_join(col("citizenship"), ", ")).alias("citizenship"),
+        col("id_transfermarkt").alias("id"),
+        col("id_thesportsdb"),
+        normalize_text_udf(col("tm_name")).alias("name"),
+        normalize_text_udf(col("tm_fullName")).alias("fullName"),
+        normalize_date_udf(col("tm_dateOfBirth")).alias("dateOfBirth"),
+        normalize_text_udf(col("tm_nameInHomeCountry")).alias("nameInHomeCountry"),
+        normalize_height_udf(col("tm_height")).alias("height"),
+        normalize_text_udf(col("tm_foot")).alias("foot"),
+        col("tm_shirtNumber").alias("shirtNumber"),
+        normalize_text_udf(normalize_currency_udf(col("tm_marketValue"))).alias("marketValue (€)"),
+        col("tm_isRetired").cast("boolean").alias("isRetired"),
+        normalize_text_udf(array_join(col("tm_citizenship"), ", ")).alias("citizenship"),
 
-        normalize_text_udf(when(col("placeOfBirth.city").isNotNull() & col("placeOfBirth.country").isNotNull(),
-            concat(col("placeOfBirth.city"), lit(", "), col("placeOfBirth.country"))
-        ).when(col("placeOfBirth.country").isNull(), 
-            col("placeOfBirth.city")
+        normalize_text_udf(when(
+            col("tm_placeOfBirth.city").isNotNull() & col("tm_placeOfBirth.country").isNotNull(),
+            concat(col("tm_placeOfBirth.city"), lit(", "), col("tm_placeOfBirth.country"))
+        ).when(col("tm_placeOfBirth.country").isNull(),
+            col("tm_placeOfBirth.city")
         ).otherwise(
-            col("placeOfBirth.country")
+            col("tm_placeOfBirth.country")
         )).alias("placeOfBirth"),
 
         normalize_text_udf(trim(regexp_replace(
-            concat_ws(", ", col("position.main"), array_join(col("position.other"), ", ")),
-            ",\\s*$", ""  # quita coma y espacios al final
+            concat_ws(", ", col("tm_position.main"), array_join(col("tm_position.other"), ", ")),
+            ",\\s*$", ""
         ))).alias("positions"),
-        
-        col("club.id").alias("clubId"),
-        normalize_text_udf(col("club.name")).alias("clubName"),
-        normalize_date_udf(col("club.joined")).alias("clubJoined"),
-        normalize_date_udf(col("club.contractExpires")).alias("contractExpires"),
+
+        col("tm_club.id").alias("clubId"),
+        normalize_text_udf(col("tm_club.name")).alias("clubName"),
+        normalize_date_udf(col("tm_club.joined")).alias("clubJoined"),
+        normalize_date_udf(col("tm_club.contractExpires")).alias("contractExpires"),
     ).fillna("")
 }
